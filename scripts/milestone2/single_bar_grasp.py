ea91a6e3226720da41f9f0b2bb42e517e68e7382
Yijiang Huang <yijiang94817@gmail.com>
2023-07-10 11:54:28 +0200

    get IK for full base working

--- scripts/milestone2/single_bar_grasp.py
+++ scripts/milestone2/single_bar_grasp.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, argparse
 import time
 import random
 import socket, json
@@ -13,6 +13,7 @@ import husky_assembly.optitrack.DataDescriptions as DataDescriptions
 import husky_assembly.optitrack.MoCapData as MoCapData
 from husky_assembly.optitrack.Utils import print_configuration
 from husky_assembly import DATA_DIRECTORY
+from tracikpy import TracIKSolver
 
 # from compas.robots import RobotModel
 # from compas_fab.robots import RobotSemantics
@@ -98,7 +99,12 @@ def load_robot():
     assert os.path.exists(gripper_obj)
     
     robot = pp.load_pybullet(robot_urdf, fixed_base=False, cylinder=True)
+
+    # TODO get tool def from SRDF
+    ik_solver = TracIKSolver(robot_urdf, "world_link", "ur_arm_tool0")
     # pp.camera_focus_on_body(robot)
+
+    # TODO get disabled collision pairs from SRDF
 
     # pp.dump_body(robot)
     # base_bb = pp.create_box(0.9864, 0.6851, 0.3767)
@@ -123,7 +129,7 @@ def load_robot():
     # tcp_pose = pp.get_link_pose(robot, pp.link_from_name(robot, 'central_tcp'))
     # pp.draw_pose(tcp_pose)
 
-    return robot, ee_attachment
+    return robot, ee_attachment, ik_solver
 
 def get_disabled_collisions(robot, disabled_self_collision_link_names):
     """get robot's link-link tuples disabled from collision checking
@@ -164,10 +170,11 @@ def check_path(joints, path, collision_fn=None, jump_threshold=None, diagnosis=F
                 return False
     return True
 
-def plan_pickup_motion(robot, current_conf, bar_body, bar_pose, attachments, obstacles, debug=False):
+def plan_pickup_motion(robot, ik_solver, current_conf, bar_body, attachments, obstacles, debug=False):
     # plan a transit motion from init conf to pick_approach conf  
     custom_limits = get_custom_limits(robot, {})
     resolutions = np.array([0.01, 0.01, 0.01, 0.01, 0.01, 0.01])
+    disabled_collisions = {}
     extra_disabled_collisions = {}
     # extra_disabled_collisions =[
     #     ((robot, pp.link_from_name(robot, 'wrist_3_link')), 
@@ -175,27 +182,43 @@ def plan_pickup_motion(robot, current_conf, bar_body, bar_pose, attachments, obs
     #     ]
 
     joints = pp.get_movable_joints(robot)
-    sample_fn = pp.get_sample_fn(robot, joints, custom_limits=custom_limits)
-    distance_fn = pp.get_distance_fn(robot, joints) #, weights=weights)
-    extend_fn = pp.get_extend_fn(robot, joints, resolutions=resolutions)
-    collision_fn = pp.get_collision_fn(robot, joints, obstacles=obstacles, attachments=attachments, 
-                                    self_collisions=True,
-                                    disabled_collisions=disabled_collisions, extra_disabled_collisions=extra_disabled_collisions,
-                                    custom_limits=custom_limits, max_distance=0)
-
-    path = []
-    start_conf = current_conf
-    end_conf = PICKUP_APPROACH_CONF
+    # sample_fn = pp.get_sample_fn(robot, joints, custom_limits=custom_limits)
+    # distance_fn = pp.get_distance_fn(robot, joints) #, weights=weights)
+    # extend_fn = pp.get_extend_fn(robot, joints, resolutions=resolutions)
+    # collision_fn = pp.get_collision_fn(robot, joints, obstacles=obstacles, attachments=attachments, 
+    #                                 self_collisions=True,
+    #                                 disabled_collisions=disabled_collisions, extra_disabled_collisions=extra_disabled_collisions,
+    #                                 custom_limits=custom_limits, max_distance=0)
+    # path = []
+    # start_conf = current_conf
+    # end_conf = PICKUP_APPROACH_CONF
 
     # TODO sample grasp and IK
 
     # grasp_gen = get_bar_grasp_gen_fn(bar_length)
+    grasp_gen = pp.get_side_cylinder_grasps(bar_body)
 
-    # for _ in range(10):
-    #     gripper_from_object = next(grasp_gen())
-    #     world_from_object = pp.multiply(tcp_pose, gripper_from_object)
-    #     pp.set_pose(bar_body, world_from_object)
-    #     pp.wait_if_gui() 
+    # joints = pp.get_movable_joints(robot)
+    joint_names = ['x', 'y', 'theta', "ur_arm_shoulder_pan_joint", "ur_arm_shoulder_lift_joint",
+                   "ur_arm_elbow_joint", "ur_arm_wrist_1_joint", "ur_arm_wrist_2_joint", "ur_arm_wrist_3_joint" ]
+    joints = pp.joints_from_names(robot, joint_names)
+
+    world_from_object = pp.get_pose(bar_body)
+    # tool0_from_ee = pp.Pose(euler=pp.Euler(yaw=-np.pi/2), point=[0,0,0.138])
+    tool0_from_ee = pp.Pose(point=[0,0,0.138])
+
+    for _ in range(1):
+        gripper_from_object = next(grasp_gen)
+        # world_from_object = pp.multiply(tcp_pose, gripper_from_object)
+        world_from_tcp_pose = pp.multiply(world_from_object, pp.invert(gripper_from_object))
+        pp.draw_pose(world_from_tcp_pose)
+        world_from_tool0 = pp.multiply(world_from_tcp_pose, pp.invert(tool0_from_ee))
+
+        conf = ik_solver.ik(pp.tform_from_pose(world_from_tool0))
+        pp.set_joint_positions(robot, joints, conf)
+        for attachment in attachments:
+            attachment.assign()
+        # pp.wait_if_gui()
 
     # with pp.LockRenderer():
     #     if pp.check_initial_end(start_conf, end_conf, collision_fn, diagnosis=debug):
@@ -230,46 +253,53 @@ def plan_pickup_motion(robot, current_conf, bar_body, bar_pose, attachments, obs
     #     path.append(transit_path[::-1])
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--disable_mocap_tracking', action='store_true',
+                        help='Disable mocap connection.')
+    parser.add_argument('--disable_joint_tracking', action='store_true',
+                        help='Disable mocap connection.')
+    args = parser.parse_args()
+
     # * create a new NatNet client
-    CLIENT_IP = '192.168.0.7' # Set to your own IP
-    MOCAP_IP = '192.168.0.117'
-    HUSKY_IP = '192.168.131.9'
-    streaming_client = NatNetClient()
-    streaming_client.set_client_address(CLIENT_IP)
-    streaming_client.set_server_address(MOCAP_IP)
-    streaming_client.set_use_multicast(False)
-    streaming_client.print_level = 0
-    # Configure the streaming client to call our rigid body handler on the emulator to send data out.
-    streaming_client.rigid_body_listener = receive_rigid_body_frame
+    if not args.disable_mocap_tracking:
+        CLIENT_IP = '192.168.0.7' # Set to your own IP
+        MOCAP_IP = '192.168.0.117'
+        HUSKY_IP = '192.168.131.9'
+        mocap_client = NatNetClient()
+        mocap_client.set_client_address(CLIENT_IP)
+        mocap_client.set_server_address(MOCAP_IP)
+        mocap_client.set_use_multicast(False)
+        mocap_client.print_level = 0
+        # Configure the streaming client to call our rigid body handler on the emulator to send data out.
+        mocap_client.rigid_body_listener = receive_rigid_body_frame
 
-    # # * create a new Husky client
-    # # HOST = '192.168.0.113'  # Standard loopback interface address (localhost)
-    # PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-    # stream_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # stream_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # # on the husky side, we set it to always send to the same port to the host
-    # stream_server.bind((CLIENT_IP, PORT))
-    # stream_server.settimeout(0.001)
+    # * create a new Husky client
+    if not args.disable_joint_tracking:
+        # HOST = '192.168.0.113'  # Standard loopback interface address (localhost)
+        PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+        joint_state_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        joint_state_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # on the husky side, we set it to always send to the same port to the host
+        joint_state_server.bind((CLIENT_IP, PORT))
+        joint_state_server.settimeout(0.001)
 
-    # # * a thread to receive data from the husky
-    # stop_thread = False
-    # stream_thread = Thread(target=socket_recv_thread, args=(stream_server, lambda : stop_thread))
-    # stream_thread.daemon = True
-    # stream_thread.start()
-
-# husky0804 (array([ 0.1854293 ,  1.16446769, -0.00768745]), array([-3.60649819e-03,  9.68992695e-04, -2.08078113e-01,  9.78105083e-01]))
-# bar ((-1.062444806098938, 0.19626910984516144, 0.6585784554481506), (0.8137449622154236, -0.1838780641555786, 0.5276390910148621, -0.1600152850151062))
-# husky0804 (array([ 0.18541652,  1.16444933, -0.00769591]), array([-3.62287471e-03,  9.76011181e-04, -2.08073338e-01,  9.78106031e-01]))
-# bar ((-1.0624510049819946, 0.19626599550247192, 0.6585925817489624), (0.813710629940033, -0.18402758240699768, 0.5276156067848206, -0.16009564697742462))
+        # * a thread to receive data from the husky
+        stop_thread = False
+        joint_state_stream_thread = Thread(target=socket_recv_thread, args=(joint_state_server, lambda : stop_thread))
+        joint_state_stream_thread.daemon = True
+        joint_state_stream_thread.start()
 
     # * start pybullet simulator
     pp.connect(use_gui=True, shadows=True, color=[0.9, 0.9, 1.0])
     p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1, physicsClientId=pp.CLIENT)
 
     # * Control UI
-    traj_param = p.addUserDebugParameter("trajectory playback", 0.0, 1.0, 0.0)
-    plan = p.addUserDebugParameter("plan", 1, 0, 0)
-    execute = p.addUserDebugParameter("execute", 1, 0, 1)
+    traj_param_slider = p.addUserDebugParameter("trajectory playback", 0.0, 1.0, 0.0)
+    #  For a button, the value of getUserDebugParameter for a button increases 1 at each button press.
+    plan_button = p.addUserDebugParameter("plan", 1, 0, 0)
+    execute_button = p.addUserDebugParameter("execute", 1, 0, 0)
+    prev_plan_button_value = p.readUserDebugParameter(plan_button)
+    prev_execute_button_value = p.readUserDebugParameter(execute_button)
 
     # * load all robots and objects
     pp.draw_pose(pp.unit_pose(), 1.0)
@@ -278,17 +308,29 @@ def main():
         # pp.create_plane(color=[0.9, 0.9, 1.0])
         bar = pp.create_cylinder(radius=0.01, height=1.0)
         with pp.HideOutput():
-            robot, ee_attachment = load_robot()
-            # a shadow robot for displaying the trajectory
-            shadow_robot, shadow_ee_attachment = load_robot()
-            shadow_color = [0.5, 0.5, 0.5, 0.3]
-            pp.set_color(shadow_robot, shadow_color)
-            pp.set_color(shadow_ee_attachment.child, shadow_color)
+            robot, ee_attachment, ik_solver = load_robot()
+
+            # ! a shadow robot for displaying the trajectory
+            # shadow_robot, shadow_ee_attachment, _ = load_robot()
+            # shadow_color = [0.5, 0.5, 0.5, 0.3]
+            # pp.set_color(shadow_robot, shadow_color)
+            # pp.set_color(shadow_ee_attachment.child, shadow_color)
+
+    obstacles = []
 
     rb_from_name = {
         'bar': bar,
         'husky0804': robot,
     }
+
+    if args.disable_mocap_tracking:
+        # a recorded pose for debuggging purpose
+        temp_bar_pose = ((-1.062444806098938, 0.19626910984516144, 0.6585784554481506), (0.8137449622154236, -0.1838780641555786, 0.5276390910148621, -0.1600152850151062))
+        pp.set_pose(bar, temp_bar_pose)
+
+    # husky0804 (array([ 0.18541652,  1.16444933, -0.00769591]), array([-3.62287471e-03,  9.76011181e-04, -2.08073338e-01,  9.78106031e-01]))
+    # bar ((-1.0624510049819946, 0.19626599550247192, 0.6585925817489624), (0.813710629940033, -0.18402758240699768, 0.5276156067848206, -0.16009564697742462))
+
     # print(pp.get_joint_positions(robot, pp.get_movable_joints(robot)))
     # pp.wait_if_gui()
     # sys.exit(0)
@@ -296,28 +338,32 @@ def main():
     try:
         # Start up the streaming client now that the callbacks are set up.
         # This will run perpetually, and operate on a separate thread.
-        is_running = streaming_client.run()
-        print_configuration(streaming_client)
-        print("\n")
-        if not is_running:
-            print("ERROR: Could not start streaming client.")
-            try:
-                sys.exit(1)
-            except SystemExit:
-                print("...")
-            finally:
-                print("exiting")
+        is_looping = False
+        if not args.disable_mocap_tracking:
+            is_running = mocap_client.run()
+            print_configuration(mocap_client)
+            print("\n")
+            if not is_running:
+                print("ERROR: Could not start streaming client.")
+                try:
+                    sys.exit(1)
+                except SystemExit:
+                    print("...")
+                finally:
+                    print("exiting")
 
-        is_looping = True
-        time.sleep(1)
-        if not streaming_client.connected():
-            print("ERROR: Could not connect properly.  Check that Motive streaming is on.")
-            try:
-                sys.exit(2)
-            except SystemExit:
-                print("...")
-            finally:
-                print("exiting")
+            is_looping = True
+            time.sleep(1)
+            if not mocap_client.connected():
+                print("ERROR: Could not connect properly.  Check that Motive streaming is on.")
+                try:
+                    sys.exit(2)
+                except SystemExit:
+                    print("...")
+                finally:
+                    print("exiting")
+
+        is_looping = is_looping | args.disable_mocap_tracking
 
         prev_handle = []
         planned_trajectory = None
@@ -342,7 +388,6 @@ def main():
                         zup_from_rb = pp.pose_from_tform(zup_tform)
                         husky_pose = zup_from_rb
 
-                    print(name, zup_from_rb)
                     rb = rb_from_name[name]
                     pp.set_pose(rb, zup_from_rb)
                     prev_handle.extend(pp.draw_pose(zup_from_rb))
@@ -357,19 +402,25 @@ def main():
             tcp_pose = pp.get_link_pose(robot, pp.link_from_name(robot, 'bar_tcp'))
             prev_handle.extend(pp.draw_pose(tcp_pose))
 
+            current_plan_button_reading = p.readUserDebugParameter(plan_button)
+            if current_plan_button_reading > prev_plan_button_value:
+                plan_pickup_motion(robot, ik_solver, None, bar, [ee_attachment], obstacles)
+                prev_plan_button_value = current_plan_button_reading
+
             if planned_trajectory:
                 # set the shadow robot to the slider value
-                traj_param_value = p.readUserDebugParameter(traj_param)
+                traj_param_value = p.readUserDebugParameter(traj_param_slider)
                 traj_idx = int(traj_param_value * (len(planned_trajectory) - 1))
                 traj_pose = planned_trajectory[traj_idx]
-                pp.set_joint_positions(shadow_robot, pp.get_movable_joints(shadow_robot), traj_pose)
-            else:
-                # hide the shadow robot
-                pp.set_pose(shadow_robot, husky_pose)
-                if arm_joint_state:
-                    joints = pp.joints_from_names(shadow_robot, arm_joint_state['name'])
-                    pp.set_joint_positions(shadow_robot, joints, arm_joint_state['position'])
-                shadow_ee_attachment.assign()
+                # pp.set_joint_positions(shadow_robot, pp.get_movable_joints(shadow_robot), traj_pose)
+
+            # else:
+            #     # hide the shadow robot
+            #     pp.set_pose(shadow_robot, husky_pose)
+            #     if arm_joint_state:
+            #         joints = pp.joints_from_names(shadow_robot, arm_joint_state['name'])
+            #         pp.set_joint_positions(shadow_robot, joints, arm_joint_state['position'])
+            #     shadow_ee_attachment.assign()
 
             time.sleep(0.01)
 
@@ -381,9 +432,12 @@ def main():
     finally:
         stop_thread = True
 
-        # stream_server.close()
-        # stream_thread.join()
-        streaming_client.shutdown()
+        if not args.disable_joint_tracking:
+            joint_state_server.close()
+            joint_state_stream_thread.join()
+
+        if not args.disable_joint_tracking:
+            mocap_client.shutdown()
 
         if pp.is_connected():
             pp.disconnect()

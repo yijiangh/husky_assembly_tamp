23ef4e49a42fefb69f4a56daac5dfb778e7d6fe9
jeong <1142835579@qq.com>
2024-12-03 15:34:50 +0800

    abort bi-level optimization

--- scripts/symbolic_planner/ik_solver/opti_solver.py
+++ scripts/symbolic_planner/ik_solver/opti_solver.py
@@ -239,6 +239,271 @@ def SymbolicForward(
         return fk_function
 
 
+class BilevelOptimization(object):
+    def __init__(
+        self,
+        upper_obj: ca.MX,
+        lower_obj: ca.MX,
+        upper_var: ca.MX,
+        lower_var: ca.MX,
+        lower_constrains,
+        lower_solver: ca.Opti,
+        upper_var_lower_param: Union[ca.MX, None] = None,
+        lower_var_upper_param: Union[ca.MX, None] = None,
+        upper_params: Union[List[ca.MX], None] = None,
+        lower_params: Union[List[ca.MX], None] = None,
+    ) -> None:
+        self.upper_obj = upper_obj
+        self.lower_obj = lower_obj
+
+        self.upper_var = upper_var
+        self.lower_var = lower_var
+
+        self.lower_constrains = lower_constrains
+        self.lower_solver = lower_solver
+
+        self.upper_var_lower_param = upper_var_lower_param
+        self.lower_var_upper_param = lower_var_upper_param
+
+        self.upper_params = upper_params
+        self.lower_params = lower_params
+
+    def eval(self, name: str, obj: ca.MX, sym: List[ca.MX], data: List[np.ndarray]) -> np.ndarray:
+        fn = ca.Function("fn", sym, [obj])
+        fn_result = fn(*data).toarray()
+        print(name, "\n", fn_result)
+        return fn_result
+
+    def solve(
+        self,
+        upper_var_init: np.ndarray,
+        lower_var_init: np.ndarray,
+        criteria: float,
+        upper_params_init: Union[List[np.ndarray], None] = None,
+        lower_params_init: Union[List[np.ndarray], None] = None,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+
+        upper_val = upper_var_init
+        lower_val = lower_var_init
+        last_upper_val = np.ones(upper_var_init.shape) * np.inf
+        last_lower_val = np.ones(lower_var_init.shape) * np.inf
+
+        # **************************************************************************
+        # Loop
+        # **************************************************************************
+
+        while (
+            np.linalg.norm(upper_val - last_upper_val) > criteria
+            and np.linalg.norm(lower_val - last_lower_val) > criteria
+        ):
+            # **************************************************************************
+            # step 1: solve lower problem
+            # **************************************************************************
+
+            # -------------------- set param --------------------#
+            if self.lower_params is not None and lower_params_init is not None:
+                for lower_param, lower_param_init in zip(self.lower_params, lower_params_init):
+                    self.lower_solver.set_value(lower_param, lower_param_init)
+
+            # -------------------- set cross param --------------------#
+            if self.upper_var_lower_param is not None:
+                self.lower_solver.set_value(self.upper_var_lower_param, upper_var_init)
+
+            # -------------------- set init --------------------#
+            self.lower_solver.set_initial(self.lower_var, lower_var_init)
+            with HideOutput():
+                lower_solution = self.lower_solver.solve()
+                lower_var_sol_k = lower_solution.value(self.lower_var)
+
+            # **************************************************************************
+            # step 2: compute derivatives of upper obj
+            # upper_var --> upper_val
+            # lower_var --> lower_var_sol_k
+            # upper_var_lower_param --> upper_val
+            # lower_var_upper_param --> lower_var_sol_k
+            # **************************************************************************
+
+            # **************************************************************************
+            # step 2.1: compute first derivative
+            # **************************************************************************
+
+            # # TODO: 这里需要去掉，但目前去掉会出现奇异矩阵
+            # upper_params_init[1] = np.array([[0, 0, 1, 0.5], [0, 1, 0, 0.25], [-1, 0, 0, 0.5], [0, 0, 0, 1]])
+            # lower_params_init[0] = np.array([[0, 0, 1, 0.5], [0, 1, 0, 0.25], [-1, 0, 0, 0.5], [0, 0, 0, 1]])
+
+            pou_pu = ca.jacobian(self.upper_obj, self.upper_var) + ca.jacobian(
+                self.upper_obj, self.upper_var_lower_param
+            )
+            pou_pl = ca.jacobian(self.upper_obj, self.lower_var_upper_param)  # TODO: 这里好像有问题
+
+            pou_pu_res = self.eval(
+                "pou_pu",
+                pou_pu,
+                [self.upper_var, self.lower_var_upper_param] + self.upper_params,
+                [upper_val, lower_var_sol_k] + upper_params_init,
+            )
+            pou_pl_res = self.eval(
+                "pou_pl",
+                pou_pl,
+                [self.upper_var, self.lower_var_upper_param] + self.upper_params,
+                [upper_val, lower_var_sol_k] + upper_params_init,
+            )
+
+            g = ca.jacobian(self.lower_obj, self.lower_var)
+            pg_pl = ca.jacobian(g, self.lower_var)
+            pg_pu = ca.jacobian(g, self.upper_var_lower_param)
+            S: ca.MX = -ca.inv(pg_pl) @ pg_pu  # dl_du
+
+            S_res = self.eval(
+                "S",
+                S,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            dou_du: ca.MX = pou_pu + pou_pl @ S
+
+            dou_du_res = self.eval(
+                "dou_du",
+                dou_du,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            # **************************************************************************
+            # step 2.2: compute second derivative
+            # **************************************************************************
+
+            # -------------------- part 1 --------------------#
+            ppou_pupu = ca.jacobian(pou_pu, self.upper_var)
+            ppou_pupl = ca.jacobian(pou_pu, self.lower_var_upper_param)
+            part_1 = ppou_pupu + ppou_pupl @ S  # part 1
+
+            ppou_pupu_res = self.eval(
+                "ppou_pupu",
+                ppou_pupu,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            ppou_pupl_res = self.eval(
+                "ppou_pupl",
+                ppou_pupl,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            part_1_res = self.eval(
+                "part_1",
+                part_1,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            # -------------------- part 2 --------------------#
+
+            # -------------------- part 2.1 --------------------#
+            ppou_plpl = ca.jacobian(pou_pl, self.lower_var_upper_param)
+            ppou_plpu = ca.jacobian(pou_pl, self.upper_var)
+            part_2_1 = S.T @ (ppou_plpl @ S + ppou_plpu)
+
+            ppou_plpl_res = self.eval(
+                "ppou_plpl",
+                ppou_plpl,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            ppou_plpu_res = self.eval(
+                "ppou_plpu",
+                ppou_plpu,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            part_2_1_res = self.eval(
+                "part_2_1",
+                part_2_1,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            # -------------------- part 2.2 --------------------#
+            n = S.shape[0]
+            m = S.shape[1]
+            G = None
+            for i in range(n):
+                block = ca.MX.eye(m) * pou_pl[i]
+                if G is None:
+                    G = block
+                else:
+                    G = ca.horzcat(G, block)
+
+            pS_pu = ca.jacobian(S, self.upper_var)
+            pS_pl = ca.jacobian(S, self.lower_var)
+            dS_du = pS_pu + pS_pl @ S
+            part_2_2 = G @ dS_du
+
+            part_2_2_res = self.eval(
+                "part_2_2",
+                part_2_2,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            ddou_dudu = part_1 + part_2_1 + part_2_2
+
+            ddou_dudu_res = self.eval(
+                "ddou_dudu",
+                ddou_dudu,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            # **************************************************************************
+            # step 3: compute direction of Newton's method
+            # **************************************************************************
+
+            d = -ca.inv(ddou_dudu) @ dou_du.T
+            d_res = self.eval(
+                "d",
+                d,
+                [self.upper_var, self.lower_var_upper_param, self.lower_var, self.upper_var_lower_param]
+                + self.upper_params
+                + self.lower_params,
+                [upper_val, lower_var_sol_k, lower_var_sol_k, upper_val] + upper_params_init + lower_params_init,
+            )
+
+            # **************************************************************************
+            # step 4: backtracking search
+            # **************************************************************************
+
+            # **************************************************************************
+            # step 5: iteration
+            # **************************************************************************
+            break
+
+
 class OptiSolver(object):
 
     MANIPULATOR_JOINT_NAMES = [
@@ -265,7 +530,7 @@ class OptiSolver(object):
 
         self.SolverInit()
 
-    def ObjectiveFunction(self, T: ca.MX, T_tar: ca.MX) -> ca.MX:
+    def IKObjectiveFunction(self, T: ca.MX, T_tar: ca.MX) -> ca.MX:
         """
         Generate objective function seperately.
 
@@ -282,30 +547,117 @@ class OptiSolver(object):
                 err += (T[i, j] - T_tar[i, j]) ** 2
         return err
 
+    def LowerObjectiveFunction(self, q: ca.MX, p: ca.MX, T_element: ca.MX) -> ca.MX:
+        # base_from_gripper, T
+        T_gripper = SymbolicForward(
+            self.urdf_path, self.REDUCED_MODEL_JOINT_NAMES, self.MANIPULATOR_JOINT_NAMES, q=q, output_type="matrix"
+        )
+
+        # element_from_gripper
+        T_delta = ca.MX.eye(4)
+        T_delta[0, 3] = p[0]
+        T_delta[1, 3] = p[1]
+
+        # base_from_gripper, T_tar
+        T_gripper_tar = T_element @ T_delta
+
+        obj = self.IKObjectiveFunction(T_gripper, T_gripper_tar)
+        return obj
+
+    def UpperObjectiveFunction(self, p: ca.MX, c: ca.MX) -> ca.MX:
+        x = p[0]
+        t = x - c
+        obj = ca.if_else(t > 0, t**2, 0)
+        return obj
+
     def SolverInit(self):
-        self.solver = ca.Opti()
+
+        # **************************************************************************
+        # solver for IK, lower solver
+        # **************************************************************************
+
+        self.ik_solver = ca.Opti()
 
         # -------------------- set parameters --------------------#
-        self.N = len(self.MANIPULATOR_JOINT_NAMES)
-        self.q = self.solver.variable(self.N)
-        self.T_tar = self.solver.parameter(4, 4)
+        self.Nq = len(self.MANIPULATOR_JOINT_NAMES)
+        self.q = self.ik_solver.variable(self.Nq)
+        self.T_tar = self.ik_solver.parameter(4, 4)  # world(arm base)_from_element
 
         # -------------------- get matrix and objective function --------------------#
         self.fk_function = SymbolicForward(self.urdf_path, self.REDUCED_MODEL_JOINT_NAMES, self.MANIPULATOR_JOINT_NAMES)
         self.fk_matrix = SymbolicForward(
             self.urdf_path, self.REDUCED_MODEL_JOINT_NAMES, self.MANIPULATOR_JOINT_NAMES, q=self.q, output_type="matrix"
-        )
-        self.obj = self.ObjectiveFunction(self.fk_matrix, self.T_tar)
+        )  # world(arm base)_from_gripper
+        self.ik_obj = self.IKObjectiveFunction(self.fk_matrix, self.T_tar)
 
         # -------------------- set solver objective function and constrains --------------------#
-        self.solver.minimize(self.obj)
-        self.solver.subject_to(ca.vec(self.q) <= np.pi)
-        self.solver.subject_to(ca.vec(self.q) >= -np.pi)
+        self.ik_solver.minimize(self.ik_obj)
+        self.ik_solver.subject_to(ca.vec(self.q) <= np.pi)
+        self.ik_solver.subject_to(ca.vec(self.q) >= -np.pi)
 
         # -------------------- set solver options --------------------#
         p_opts = {"expand": True, "print_time": 0}
         s_opts = {"max_iter": 100, "print_level": 0}
-        self.solver.solver("ipopt", p_opts, s_opts)
+        self.ik_solver.solver("ipopt", p_opts, s_opts)
+
+        # **************************************************************************
+        # solver for Bi-level optimization:
+        #   O_{U} --> O_{grasp}
+        #   O_{L} --> IK objective function
+        # **************************************************************************
+
+        self.lower_solver = ca.Opti()
+        self.upper_solver = ca.Opti()
+
+        # -------------------- set variables --------------------#
+        self.lower_var_q = self.lower_solver.variable(self.Nq)  # q
+        self.upper_var_p = self.upper_solver.variable(2)  # p: (x, y, theta)
+
+        # -------------------- set cross parameters --------------------#
+        self.lower_var_upper_param_q = self.upper_solver.parameter(self.Nq)
+        self.upper_var_lower_param_p = self.lower_solver.parameter(2)  # p: (x, y, theta)
+
+        # -------------------- set parameters --------------------#
+        self.lower_param_T_element = self.lower_solver.parameter(4, 4)
+
+        self.upper_param_c = self.upper_solver.parameter(1)
+        self.upper_param_T_element = self.upper_solver.parameter(4, 4)
+
+        # -------------------- set objective functions --------------------#
+        self.lower_obj = self.LowerObjectiveFunction(
+            self.lower_var_q, self.upper_var_lower_param_p, self.lower_param_T_element
+        )
+        # self.upper_obj = self.UpperObjectiveFunction(
+        #     self.upper_var_p, self.upper_param_c
+        # ) + self.LowerObjectiveFunction(self.lower_var_upper_param_q, self.upper_var_p, self.upper_param_T_element)
+        # self.upper_obj = self.UpperObjectiveFunction(self.upper_var_p, self.upper_param_c)
+        self.upper_obj = self.LowerObjectiveFunction(
+            self.lower_var_upper_param_q, self.upper_var_p, self.upper_param_T_element
+        )
+
+        self.lower_solver.minimize(self.lower_obj)
+        self.upper_solver.minimize(self.upper_obj)
+
+        self.lower_solver.subject_to(ca.vec(self.lower_var_q) <= np.pi)
+        self.lower_solver.subject_to(ca.vec(self.lower_var_q) >= -np.pi)
+
+        # -------------------- set solver options --------------------#
+        p_opts = {"expand": True, "print_time": 0}
+        s_opts = {"max_iter": 100, "print_level": 0}
+        self.lower_solver.solver("ipopt", p_opts, s_opts)
+
+        self.bilevel_solver = BilevelOptimization(
+            self.upper_obj,
+            self.lower_obj,
+            self.upper_var_p,
+            self.lower_var_q,
+            None,
+            self.lower_solver,
+            upper_var_lower_param=self.upper_var_lower_param_p,
+            lower_var_upper_param=self.lower_var_upper_param_q,
+            upper_params=[self.upper_param_c, self.upper_param_T_element],
+            lower_params=[self.lower_param_T_element],
+        )
 
     def forward(self, q: Union[np.ndarray, List[float]]) -> np.ndarray:
 
@@ -337,10 +689,14 @@ class OptiSolver(object):
             success (bool, [optional]): solve status
         """
 
-        self.solver.set_value(self.T_tar, pose)
-        self.solver.set_initial(self.q, qinit)
+        self.ik_solver.set_value(self.T_tar, pose)
+        self.ik_solver.set_initial(self.q, qinit)
         with HideOutput():
-            solution = self.solver.solve()
+            solution = self.ik_solver.solve()
+
+        pinit = np.array([1.0, 0])
+
+        self.bilevel_solver.solve(pinit, qinit, 0.1, upper_params_init=[0.5, pose], lower_params_init=[pose])
         return np.array(solution.value(self.q))
 
 
@@ -408,7 +764,7 @@ if __name__ == "__main__":
     base_from_tip = np.array([[0, 0, 1, 0.5], [0, 1, 0, 0.25], [-1, 0, 0, 0.5], [0, 0, 0, 1]])
 
     q_pin = pin_ik_solver_relative(
-        base_from_tip, qinit=np.array([-1.29891436, 0.3618943, -1.79107962, -1.71236292, -1.84276683, 1.57065429])
+        base_from_tip, qinit=np.array([0, 0, 0, 0, 0, 0])
     )
 
     print("> ik solver: q_pin")
@@ -446,7 +802,7 @@ if __name__ == "__main__":
     print(q_pin)
     print()
 
-    q_opti = opti_ik_solver.ik(target_pose, qinit=np.array([0, 0, 0, 0, 0, 0]))
+    q_opti = opti_ik_solver.ik(target_pose, qinit=q_pin)
     print("> ik solver: q_opti")
     print(q_opti)
     print()

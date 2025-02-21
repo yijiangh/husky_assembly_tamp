4aa0441e5775c4a51731516b063ea89b06127ba9
jeong <1142835579@qq.com>
2025-02-21 15:43:48 +0800

    start moveit in launch

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # husky-assembly
 
-## TODO
-* [x] other obstacles 好像不起作用
-* [x] back to init 应该单独拆分成一个模块，或者使用transfer来规划
-* [x] back to init 应该在cooperation motion plan的末尾
-* [x] robot pose sampler 应该考虑collision
-* [x] 把颜色打印添加到一个单独的模块
-* [x] place模块应该考虑到multi-assemble的情况，即在end conf的时候不应该挡住别的
-* [ ] group motion plan在失败时会报错
+## Note
+
+* 如果使用WSL，在Rviz中不能正常显示mesh，可以采取下面的方案：
+  * 方案1：```export LIBGL_ALWAYS_SOFTWARE=1```
+  * 方案2：[参考链接](https://blog.csdn.net/GodNotAMen/article/details/125123186)
+* urdf无法正常加载：```export ROS_PACKAGE_PATH=<path_to_husky_assembly>/data/husky_urdf:$ROS_PACKAGE_PATH```
--- scripts/visualizer/svsdf_visualizer.py
+++ scripts/visualizer/svsdf_visualizer.py
@@ -1,27 +1,34 @@
 if __name__ == "__main__":
-    import os
-    import sys
-    import pybullet_planning as pp
-    import numpy as np
 
-    HERE = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    sys.path.append(HERE)
+    pass
 
-    import utils.load_multi_tangent as load_multi_tangent
-    from multi_tangent.collision import create_collision_bodies
-    from multi_tangent.convert import flatten_list
-    from robot.robot_setup import RobotSetup
-    from utils.collision import Element, create_couplers, init_pb
+    # **************************************************************************
+    # open pybullet to visualize
+    # **************************************************************************
 
-    urdf_path = (
-        "/home/jeong/summer_research/eth/husky_assembly/data/husky_urdf/mt_husky_moveit_config/urdf/husky_ur5_e.urdf"
-    )
+    # import os
+    # import sys
+    # import pybullet_planning as pp
+    # import numpy as np
 
-    init_pb()
+    # HERE = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    # sys.path.append(HERE)
 
-    rb = RobotSetup("r0")
-    rb.set_joint_positions(rb.arm_joints, np.array([0] * 6))
+    # import utils.load_multi_tangent as load_multi_tangent
+    # from multi_tangent.collision import create_collision_bodies
+    # from multi_tangent.convert import flatten_list
+    # from robot.robot_setup import RobotSetup
+    # from utils.collision import Element, create_couplers, init_pb
 
-    pp.draw_pose(pp.get_pose(rb.ee_attachment.child))
+    # urdf_path = (
+    #     "/home/jeong/summer_research/eth/husky_assembly/data/husky_urdf/mt_husky_moveit_config/urdf/husky_ur5_e.urdf"
+    # )
 
-    pp.wait_for_user()
+    # init_pb()
+
+    # rb = RobotSetup("r0")
+    # rb.set_joint_positions(rb.arm_joints, np.array([0] * 6))
+
+    # pp.draw_pose(pp.get_pose(rb.ee_attachment.child))
+
+    # pp.wait_for_user()
